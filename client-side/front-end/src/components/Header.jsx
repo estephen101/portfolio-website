0c7fa1d5d44ed983a3d1114848e51d14d42ff97e
estephen101 <eugenestephen101@gmail.com>
2025-02-24 19:49:10 +0300

    set the project structure and added simple header file

---
+++ client-side/front-end/src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+import {Link}  from 'react-router-dom'
+
+function Header (){
+    return(
+        <div>
+            <h1>Portfolio</h1>
+            <nav>
+                <ul>
+                    <li><LInk to="/">Home</LInk></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Header;
